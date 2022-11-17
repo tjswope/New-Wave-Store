92f1b6eed459e20cfc770e90c416c2154a1e9d96
Tom Swope <tjswope@gmail.com>
2022-11-17 13:18:18 -0500

    Initial Commit

---
+++ New Wave Store/New_Wave_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_Wave_StoreApp.swift
+//  New Wave Store
+//
+//  Created by Swope, Thomas on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct New_Wave_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
